16c27a97749e99bce595b2731dc0a091d56edde1
bglmmz <lvxiaoyi@juzix.net>
2020-12-29 16:22:48 +0800

    refactor code for unit test, add run(BaseOptionParser parser);

--- mtool-client/src/main/java/com/platon/mtool/client/MtoolClient.java
+++ mtool-client/src/main/java/com/platon/mtool/client/MtoolClient.java
@@ -63,23 +63,23 @@ public class MtoolClient {
       if (e.getMessage() != null) {
         if (e.getMessage().contains("Was passed main parameter")) {
           String arg =
-              e.getMessage()
-                  .replace("Was passed main parameter '", "")
-                  .replace("' but no main parameter was defined in your arg class", "");
+                  e.getMessage()
+                          .replace("Was passed main parameter '", "")
+                          .replace("' but no main parameter was defined in your arg class", "");
           PrintUtils.echo(
-              "command '%s' not support option '%s'", e.getJCommander().getParsedCommand(), arg);
+                  "command '%s' not support option '%s'", e.getJCommander().getParsedCommand(), arg);
         } else if (e.getMessage().contains("The following options are required")) {
           PrintUtils.echo(
-              e.getMessage()
-                  .replace("The following options are required", "Missing required option"));
+                  e.getMessage()
+                          .replace("The following options are required", "Missing required option"));
         } else if (e.getMessage().contains("The following option is required")) {
           PrintUtils.echo(
-              e.getMessage()
-                  .replace("The following option is required", "Missing required option"));
+                  e.getMessage()
+                          .replace("The following option is required", "Missing required option"));
         } else if (e.getMessage().contains("Expected a value after parameter")) {
           PrintUtils.echo(
-              e.getMessage().replace("Expected a value after parameter", "").trim()
-                  + " can't be empty");
+                  e.getMessage().replace("Expected a value after parameter", "").trim()
+                          + " can't be empty");
         } else {
           PrintUtils.echo(e.getMessage());
         }
@@ -95,6 +95,10 @@ public class MtoolClient {
       PrintUtils.echo(sb.toString());
       System.exit(0);
     }
+    run(parser);
+  }
+
+  public void run(BaseOptionParser parser){
 
     if (StringUtils.isEmpty(parser.getCommandName())) {
       // 如果命令命令， 看看选项是否是输出帮助或者输出版本
@@ -125,7 +129,7 @@ public class MtoolClient {
           // 条件判断传入的钱包是否观察钱包， 执行对应的执行器
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveStakingExecutor(parser.getSubCommander(), parser.getStakingOption());
+                    new ObserveStakingExecutor(parser.getSubCommander(), parser.getStakingOption());
           } else {
             cliExecutor = new StakingExecutor(parser.getSubCommander(), parser.getStakingOption());
           }
@@ -134,158 +138,158 @@ public class MtoolClient {
           keystore = parser.getDeclareVersionOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveDeclareVersionExecutor(
-                    parser.getSubCommander(), parser.getDeclareVersionOption());
+                    new ObserveDeclareVersionExecutor(
+                            parser.getSubCommander(), parser.getDeclareVersionOption());
           } else {
             cliExecutor =
-                new DeclareVersionExecutor(
-                    parser.getSubCommander(), parser.getDeclareVersionOption());
+                    new DeclareVersionExecutor(
+                            parser.getSubCommander(), parser.getDeclareVersionOption());
           }
           break;
         case AllCommands.INCREASESTAKING:
           keystore = parser.getIncreaseStakingOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveIncreaseStakingExecutor(
-                    parser.getSubCommander(), parser.getIncreaseStakingOption());
+                    new ObserveIncreaseStakingExecutor(
+                            parser.getSubCommander(), parser.getIncreaseStakingOption());
           } else {
             cliExecutor =
-                new IncreaseStakingExecutor(
-                    parser.getSubCommander(), parser.getIncreaseStakingOption());
+                    new IncreaseStakingExecutor(
+                            parser.getSubCommander(), parser.getIncreaseStakingOption());
           }
           break;
         case AllCommands.SUBMIT_VERSION_PROPOSAL:
           keystore = parser.getSubmitVersionProposalOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveSubmitVersionProposalExecutor(
-                    parser.getSubCommander(), parser.getSubmitVersionProposalOption());
+                    new ObserveSubmitVersionProposalExecutor(
+                            parser.getSubCommander(), parser.getSubmitVersionProposalOption());
           } else {
             cliExecutor =
-                new SubmitVersionProposalExecutor(
-                    parser.getSubCommander(), parser.getSubmitVersionProposalOption());
+                    new SubmitVersionProposalExecutor(
+                            parser.getSubCommander(), parser.getSubmitVersionProposalOption());
           }
           break;
         case AllCommands.SUBMIT_TEXT_PROPOSAL:
           keystore = parser.getSubmitTextProposalOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveSubmitTextproposalExecutor(
-                    parser.getSubCommander(), parser.getSubmitTextProposalOption());
+                    new ObserveSubmitTextproposalExecutor(
+                            parser.getSubCommander(), parser.getSubmitTextProposalOption());
           } else {
             cliExecutor =
-                new SubmitTextProposalExecutor(
-                    parser.getSubCommander(), parser.getSubmitTextProposalOption());
+                    new SubmitTextProposalExecutor(
+                            parser.getSubCommander(), parser.getSubmitTextProposalOption());
           }
           break;
         case AllCommands.SUBMIT_CANCEL_PROPOSAL:
           keystore = parser.getSubmitCancelProposalOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveSubmitCancelproposalExecutor(
-                    parser.getSubCommander(), parser.getSubmitCancelProposalOption());
+                    new ObserveSubmitCancelproposalExecutor(
+                            parser.getSubCommander(), parser.getSubmitCancelProposalOption());
           } else {
             cliExecutor =
-                new SubmitCancelProposalExecutor(
-                    parser.getSubCommander(), parser.getSubmitCancelProposalOption());
+                    new SubmitCancelProposalExecutor(
+                            parser.getSubCommander(), parser.getSubmitCancelProposalOption());
           }
           break;
         case AllCommands.SUBMIT_PARAM_PROPOSAL:
           keystore = parser.getSubmitParamProposalOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveSubmitParamProposalExecutor(
-                    parser.getSubCommander(), parser.getSubmitParamProposalOption());
+                    new ObserveSubmitParamProposalExecutor(
+                            parser.getSubCommander(), parser.getSubmitParamProposalOption());
           } else {
             cliExecutor =
-                new SubmitParamProposalExecutor(
-                    parser.getSubCommander(), parser.getSubmitParamProposalOption());
+                    new SubmitParamProposalExecutor(
+                            parser.getSubCommander(), parser.getSubmitParamProposalOption());
           }
           break;
         case AllCommands.UNSTAKING:
           keystore = parser.getUnstakingOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveUnstakingExecutor(parser.getSubCommander(), parser.getUnstakingOption());
+                    new ObserveUnstakingExecutor(parser.getSubCommander(), parser.getUnstakingOption());
           } else {
             cliExecutor =
-                new UnstakingExecutor(parser.getSubCommander(), parser.getUnstakingOption());
+                    new UnstakingExecutor(parser.getSubCommander(), parser.getUnstakingOption());
           }
           break;
         case AllCommands.UPDATE_VALIDATOR:
           keystore = parser.getUpdateValidatorOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveUpdateValidatorExecutor(
-                    parser.getSubCommander(), parser.getUpdateValidatorOption());
+                    new ObserveUpdateValidatorExecutor(
+                            parser.getSubCommander(), parser.getUpdateValidatorOption());
           } else {
             cliExecutor =
-                new UpdateValidatorExecutor(
-                    parser.getSubCommander(), parser.getUpdateValidatorOption());
+                    new UpdateValidatorExecutor(
+                            parser.getSubCommander(), parser.getUpdateValidatorOption());
           }
           break;
         case AllCommands.VOTE_VERSION_PROPOSAL:
           keystore = parser.getVoteVersionProposalOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveVoteVersionProposalExecutor(
-                    parser.getSubCommander(), parser.getVoteVersionProposalOption());
+                    new ObserveVoteVersionProposalExecutor(
+                            parser.getSubCommander(), parser.getVoteVersionProposalOption());
           } else {
             cliExecutor =
-                new VoteVersionProposalExecutor(
-                    parser.getSubCommander(), parser.getVoteVersionProposalOption());
+                    new VoteVersionProposalExecutor(
+                            parser.getSubCommander(), parser.getVoteVersionProposalOption());
           }
           break;
         case AllCommands.VOTE_TEXT_PROPOSAL:
           keystore = parser.getVoteTextProposalOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveVoteTextProposalExecutor(
-                    parser.getSubCommander(), parser.getVoteTextProposalOption());
+                    new ObserveVoteTextProposalExecutor(
+                            parser.getSubCommander(), parser.getVoteTextProposalOption());
           } else {
             cliExecutor =
-                new VoteTextProposalExecutor(
-                    parser.getSubCommander(), parser.getVoteTextProposalOption());
+                    new VoteTextProposalExecutor(
+                            parser.getSubCommander(), parser.getVoteTextProposalOption());
           }
           break;
         case AllCommands.VOTE_CANCEL_PROPOSAL:
           keystore = parser.getVoteCancelProposalOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveVoteCancelProposalExecutor(
-                    parser.getSubCommander(), parser.getVoteCancelProposalOption());
+                    new ObserveVoteCancelProposalExecutor(
+                            parser.getSubCommander(), parser.getVoteCancelProposalOption());
           } else {
             cliExecutor =
-                new VoteCancelProposalExecutor(
-                    parser.getSubCommander(), parser.getVoteCancelProposalOption());
+                    new VoteCancelProposalExecutor(
+                            parser.getSubCommander(), parser.getVoteCancelProposalOption());
           }
           break;
         case AllCommands.VOTE_PARAM_PROPOSAL:
           keystore = parser.getVoteParamProposalOption().getKeystore();
           if (keystore != null && keystore.getType().equals(Keystore.Type.OBSERVE)) {
             cliExecutor =
-                new ObserveVoteParamProposalExecutor(
-                    parser.getSubCommander(), parser.getVoteParamProposalOption());
+                    new ObserveVoteParamProposalExecutor(
+                            parser.getSubCommander(), parser.getVoteParamProposalOption());
           } else {
             cliExecutor =
-                new VoteParamProposalExecutor(
-                    parser.getSubCommander(), parser.getVoteParamProposalOption());
+                    new VoteParamProposalExecutor(
+                            parser.getSubCommander(), parser.getVoteParamProposalOption());
           }
           break;
         case AllCommands.CREATE_OBSERVEWALLET:
           cliExecutor =
-              new CreateObserveWalletExecutor(
-                  parser.getSubCommander(), parser.getCreateObserveWalletOption());
+                  new CreateObserveWalletExecutor(
+                          parser.getSubCommander(), parser.getCreateObserveWalletOption());
           break;
         case AllCommands.OFFLINESIGN:
           cliExecutor =
-              new OfflineSignExcutor(parser.getSubCommander(), parser.getOfflineSignOption());
+                  new OfflineSignExcutor(parser.getSubCommander(), parser.getOfflineSignOption());
           break;
         case AllCommands.SEND_SIGNEDTX:
           cliExecutor =
-              new SendSignedTxExecutor(parser.getSubCommander(), parser.getSendSignedTxOption());
+                  new SendSignedTxExecutor(parser.getSubCommander(), parser.getSendSignedTxOption());
           break;
-          // 模块命令会传入相应的路由解析， 比普通命令多一层路由
+        // 模块命令会传入相应的路由解析， 比普通命令多一层路由
         case AllModules.ACCOUNT:
           cliExecutor = new AccountRouter(parser.getSubCommander(), parser.getAccountOptions());
           break;
@@ -298,4 +302,5 @@ public class MtoolClient {
       cliExecutor.execute();
     }
   }
+
 }
